ed1764a1478726c33270139869609d6b2b23e528
marioluckiluke <bdfnatsu@gmail.com>
2023-06-20 11:30:28 +0700

    feat: added UI for first section

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Html, useProgress } from '@react-three/drei';
 
-const Loader = () => {
-  return <div>Loader</div>
-}
+const Loader: React.FC = () => {
+  const { progress } = useProgress();
+  return (
+    <Html>
+      <span className='canvas-load'></span>
+      <p className='text-[14px] text-[#f1f1f1] font-[800] mt-[40px]'>{progress.toFixed(2)}%</p>
+    </Html>
+  );
+};
 
-export default Loader
+export default Loader;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar: React.FC = () => {
   const [toggle, setToggle] = useState<boolean>(false);
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 left-0 z-20 bg-primary`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
           to='/'
