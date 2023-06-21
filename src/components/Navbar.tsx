7faa98bf393b0b163a3fc4057daad50394fcb933
marioluckiluke <bdfnatsu@gmail.com>
2023-06-21 16:05:40 +0700

    fix: fix three

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar: React.FC = () => {
         >
           <img src={dhnlogo} alt='Logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex'>
-            Nam Dang &nbsp; <span className='sm:block hidden'>| ReactJS Mastery</span>
+            Nam Dang &nbsp; <span className='sm:block hidden'>| Front-end Developer</span>
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
