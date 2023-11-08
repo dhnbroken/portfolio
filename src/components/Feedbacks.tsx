c6d66654bbc71e2584c23661238774153807f5c1
dhnbroken <bdfnatsu@gmail.com>
2023-11-08 21:24:35 +0700

    chore: resolve build failed

--- src/components/Feedbacks.tsx
+++ src/components/Feedbacks.tsx
@@ -1,46 +1,45 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 
 import { styles } from '~/styles';
-import { fadeIn, textVariant } from '../utils/motion.js';
+import { textVariant } from '../utils/motion.js';
 // import { testimonials } from '~/constants';
 
 import { SectionWrapper } from '~/hoc';
 
-interface IFeedBackCardProps {
-  index: number;
-  testimonial: string;
-  name: string;
-  designation: string;
-  company: string;
-  image: string;
-}
+// interface IFeedBackCardProps {
+//   index: number;
+//   testimonial: string;
+//   name: string;
+//   designation: string;
+//   company: string;
+//   image: string;
+// }
 
-const FeedbackCard: React.FC<IFeedBackCardProps> = ({ index, testimonial, name, designation, company, image }) => (
-  <motion.div
-    variants={fadeIn('', 'spring', index * 0.5, 0.75)}
-    className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
-  >
-    <p className='text-white font-black text-[48px]'>&quot;</p>
+// const FeedbackCard: React.FC<IFeedBackCardProps> = ({ index, testimonial, name, designation, company, image }) => (
+//   <motion.div
+//     variants={fadeIn('', 'spring', index * 0.5, 0.75)}
+//     className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+//   >
+//     <p className='text-white font-black text-[48px]'>&quot;</p>
 
-    <div className='mt-1'>
-      <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
+//     <div className='mt-1'>
+//       <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
 
-      <div className='mt-7 flex justify-between items-center gap-1'>
-        <div className='flex-1 flex flex-col'>
-          <p className='text-white font-medium text-[16px]'>
-            <span className='blue-text-gradient'>@</span>
-            {name}
-          </p>
-          <p className='mt-1 text-secondary text-[12px]'>
-            {designation} of {company}
-          </p>
-        </div>
-        <img src={image} alt={`feedback-by-${name}`} className='w-10 h-10 rounded-full object-cover' />
-      </div>
-    </div>
-  </motion.div>
-);
+//       <div className='mt-7 flex justify-between items-center gap-1'>
+//         <div className='flex-1 flex flex-col'>
+//           <p className='text-white font-medium text-[16px]'>
+//             <span className='blue-text-gradient'>@</span>
+//             {name}
+//           </p>
+//           <p className='mt-1 text-secondary text-[12px]'>
+//             {designation} of {company}
+//           </p>
+//         </div>
+//         <img src={image} alt={`feedback-by-${name}`} className='w-10 h-10 rounded-full object-cover' />
+//       </div>
+//     </div>
+//   </motion.div>
+// );
 
 const Feedbacks = () => {
   return (
